5506e5336fe604b94dff6f0ccd71ce16252f6a26
Davide Rodo <davide.rodo@pdave.dev>
2025-10-03 15:29:39 +0200

    Add additional allowed hosts in Vite server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   server: {
-    allowedHosts: [".pdave.dev"],
+    allowedHosts: [".pdave.dev", "localhost", ".github.io"],
   },
 });
